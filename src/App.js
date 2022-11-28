61bf2b37af6797e28ab75554e11c5b058e8754aa
Cameron Weijmer <weijmer.c@gmail.com>
2022-11-28 16:40:22 +0100

    Added styling and HTML

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from "./MyComponent.js";
+import MyButton from "./MyButton";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <MyComponent/>
+            <MyButton/>
+        </div>
+    )
 }
+
+// <div className="App">
+//     <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo"/>
+//         <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//         >
+//             Learn React, this way
+//         </a>
+//     </header>
+// </div>
+// )
+// ;
+// }
 
 export default App;
