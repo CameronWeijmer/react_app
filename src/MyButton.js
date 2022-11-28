6b81ac523a30f3690642804b7b71d2b4c34ddc23
Cameron Weijmer <weijmer.c@gmail.com>
2022-11-28 16:39:18 +0100

    Added Button

---
+++ src/MyButton.js
@@ -0,0 +1,26 @@
+export default function MyButton() {
+    function onClick() {
+        alert("You clicked me!");
+    }
+
+    function onClick2(e) {
+        if (e.ctrlKey === true) {
+            alert("Button clicked and Ctrl pressed. " + e.ctrlKey)
+        } else {
+            alert("Only Button clicked. " + e.ctrlKey)
+        }
+    }
+
+    return (
+        <div>
+            <div class={"myButton"}>
+                <h1>My Button</h1>
+                <button onClick={onClick}>Click me!</button>
+            </div>
+            <div className={"myButton"}>
+                <h1>My Second Button </h1>
+                <button onClick={(e) => onClick2(e)}>Click me another!</button>
+            </div>
+        </div>
+    );
+}
