10455a5248f08b5a4b0a17dac8ec6fafadc60f2c
Cameron Weijmer <weijmer.c@gmail.com>
2022-12-05 16:57:02 +0100

    🎨 Formatted Code

--- src/VisitorFeedback.js
+++ src/VisitorFeedback.js
@@ -1,25 +1,27 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function MyVisitorFeedback() {
-    const [feedback, setFeedback] = useState("");
-    const [records, setRecord] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [records, setRecord] = useState("");
 
-    function onChangeText(event) {
-        setFeedback(event.target.value)
-    }
+  function onChangeText(event) {
+    setFeedback(event.target.value);
+  }
 
-    function onUpdateRecords() {
-        setRecord(records + feedback + "\n")
-    }
+  function onUpdateRecords() {
+    setRecord(records + feedback + "\n");
+  }
 
-    return (
-        <div>
-            <h2>Visitor feedback</h2>
-            <p>Please leave your feedback. Thank you!</p>
-            <label>New feedback:&nbsp; <input type="text" value={feedback} onChange={(e) => onChangeText(e)}/>
-            </label>
-            <button onClick={onUpdateRecords}>add</button>
-            <p style={{whiteSpace: "pre-line"}}>{records}</p>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Visitor feedback</h2>
+      <p>Please leave your feedback. Thank you!</p>
+      <label>
+        New feedback:&nbsp;{" "}
+        <input type="text" value={feedback} onChange={(e) => onChangeText(e)} />
+      </label>
+      <button onClick={onUpdateRecords}>add</button>
+      <p style={{ whiteSpace: "pre-line" }}>{records}</p>
+    </div>
+  );
 }
